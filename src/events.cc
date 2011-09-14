a4ee227efeee64c3a71912e5d123f60b659c2392
CrypticSwarm <crypticswarm+git@gmail.com>
2011-09-14 01:28:34 -0500

    events no longer requires config.h

--- src/events.cc
+++ src/events.cc
@@ -1,39 +1,54 @@
 #include <node.h>
 #include <string.h>
 #include <xcb/xcb.h>
-#include <unistd.h>
 #include <iostream>
-#include <vector>
+#include <functional>
 #include "__autogen_events.h"
-#include "config.h"
 
 namespace XCBJS {
 
 using namespace v8;
 
-class Event {
+class Event : node::ObjectWrap {
+private:
+  struct EventData {
+    xcb_connection_t *connection;
+    xcb_generic_event_t *ev;
+  };
 public:
 
   static void Init(Handle<Object> target) {
     HandleScope scope;
     InitXCB2JSEvents(Persistent<Object>::New(target));
-    eio_custom(Event::LookForEvent, EIO_PRI_DEFAULT, Event::EventObtained, NULL);
+  }
+
+  Event(xcb_connection_t * c) : connection(c) {
+    eio_custom(LookForEvent, EIO_PRI_DEFAULT, EventObtained, PrepEventData(connection));
     ev_ref(EV_DEFAULT_UC);
   }
 
+  static EventData *PrepEventData(xcb_connection_t *connection) {
+    EventData *ev = new EventData;
+    ev->connection = connection;
+    return ev;
+  }
+
   static int EventObtained(eio_req *req) {
-    eio_custom(Event::LookForEvent, EIO_PRI_DEFAULT, Event::EventObtained, NULL);
-    xcb_generic_event_t *ev = (xcb_generic_event_t*)req->data;
-    distributeEvent(ev);
+    EventData *ev = static_cast<EventData*>(req->data);
+    eio_custom(LookForEvent, EIO_PRI_DEFAULT, EventObtained, PrepEventData(ev->connection));
+    distributeEvent(ev->ev);
+    delete ev;
     return 0;
   }
 
   static int LookForEvent(eio_req *req) {
-    xcb_generic_event_t *event;
-    event = xcb_wait_for_event(Config::connection);
-    req->data = event;
+    EventData *ev = static_cast<EventData*>(req->data);
+    ev->ev = xcb_wait_for_event(ev->connection);
     return req->result = 0;
   }
+
+private:
+  xcb_connection_t *connection;
 
 };
 
